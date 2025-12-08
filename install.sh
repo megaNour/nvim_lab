042545c22d2043b929b95b13c0f232a3ac892e52
megaNour <alkotobnour@gmail.com>
2025-12-08 10:41:32 +0100

    make compose work from everywhere and update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ ENTRY=$(CDPATH='' cd -- "$(dirname "$0")" && pwd)
 
 target=$HOME/.local/bin/lab
 printf '%s\n' '#!/bin/sh' >"$target"
-printf 'podman compose -f "%s" run --rm -it "$@" lab\n' "$ENTRY/compose.yaml" >>"$target"
+printf 'ENTRY=%s podman compose -f "%s" run --rm -it "$@" lab\n' "$ENTRY" "$ENTRY/compose.yaml" >>"$target"
 chmod 744 "$target"
